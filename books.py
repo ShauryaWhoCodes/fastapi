ebb44df9377474cea5b052dc7d92a80548816f99
Shaurya Mahajan <shauryamahajan27@gmail.com>
2026-06-25 23:17:02 +0530

    Books list api

--- books.py
+++ books.py
@@ -2,6 +2,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/hello-world")
 async def first_api():
     return {"message": "Hello World!!"}
+
+
+Books = [
+    {
+        "id": 1,
+        "title": "The Book",
+        "author": "Joe Stantham",
+        "subject": "History"},
+    {
+        "id": 2,
+        "title": "The Alchemy Book",
+        "author": "Danny Mathur",
+        "subject": "Maths"},
+    {
+        "id": 3,
+        "title": "Focus",
+        "author": "Freddy",
+        "subject": "Philosophy"
+    }
+]
+
+
+@app.get("/books")
+async def get_books():
+    return Books
