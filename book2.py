67ea33e3894e0a1e196753618868f09d24812a2d
Shaurya Mahajan <shauryamahajan27@gmail.com>
2026-06-28 11:23:12 +0530

    Project 2 Books

---
+++ book2.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: float
+
+    def __init__(self, id: int, title: str, author: str, description: str, rating: float):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+Books = [
+    Book(1, 'Book1', author='Author1', description='Description1', rating=5.0),
+    Book(2, 'Book2', author='Author2', description='Description2', rating=3.0),
+    Book(3, 'Book3', author='Author3', description='Description3', rating=2.0),
+    Book(4, 'Book4', author='Author4', description='Description4', rating=4.0),
+    Book(5, 'Book5', author='Author5', description='Description5', rating=1.0),
+]
+
+@app.get("/read_all_books")
+def read_all_books():
+    return Books
